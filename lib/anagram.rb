232c37c0b8f2ee4d3d0679252fa9945bff43c8d5
Yejin Na <101006019+JustEddie@users.noreply.github.com>
2022-09-15 18:20:26 +1000

    commit tree 3ef5f33b3fda45aee1f25dee2354a0e2ceed0668

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+
+    attr_accessor :word
+
+    def initialize (word)
+        @word = word
+    end
+
+    def match(sentence)
+        puts "find #{word} in #{sentence}"
+        sentence.find_all do |x| 
+            if x.split("").sort == word.split("").sort 
+              x 
+            end
+        end
+    end
+end
